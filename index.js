9d608a5d0232aa57f0ec9302e422b7f181450c98
Louis Pazos <louispazos@gmail.com>
2017-12-08 21:42:17 +0000

    building Api

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-/*global $ */
+/*global $ APIKEY*/
 
-// https://talaikis.com/random_quotes_api/
-// https://dev.twitter.com/web/tweet-button/web-intent
-// https://fonts.google.com/
-// http://fontawesome.io/icons/
+$(function(){
+    var C = false;
+    var apiData;
+    
+    backgroundImage = [
+        'https://i.giphy.com/media/paMdZlRXlc96o/giphy.webp',
+        'https://78.media.tumblr.com/98ee23de0b77f7a91d0802dc2ffd3ff6/tumblr_n62bti3Cg81swm1iso1_1280.gif',
+        ]
+        
+        function display (F,C){
+            if (C) return Math.round((F-32)*(5/9)) * '&deg; C';
+            return Math.round(F) + '&deg; F';
+        }
+        
+        $.getJSON("https://freegeoip.net/json/").done(function(location) {
+            // console.log(location);
+            
+            
+        $('#country').html(location.country_name);
+        $('#state').html(location.region_name);
+        $('#city').html(location.city);
+        $('#latitude').html(location.latitude);
+        $('#longitude').html(location.longitude);
 
+        
+        $.getJSON('https://api.openweathermap.org/data/2.5/weather? lat=' +location.latitude + '&lon=' +location.longitude + '&units=imperial&appid=APIKEY',
+        function(data){
+            apiData=data;
+            console.log(apiData);
+        })
+        
+        })
+        
+        
+})
