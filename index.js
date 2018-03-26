c8597ac18330abad583b57fb426b52e26707ea83
Louis Pazos <louispazos@gmail.com>
2018-03-26 20:27:05 +0000

    able to display

--- index.js
+++ index.js
@@ -4,19 +4,34 @@ $(function(){
     var C = false;
     var apiData;
     
-    backgroundImage = [ 
-        'https://i.giphy.com/media/paMdZlRXlc96o/giphy.webp',
-        'https://78.media.tumblr.com/98ee23de0b77f7a91d0802dc2ffd3ff6/tumblr_n62bti3Cg81swm1iso1_1280.gif',
+    // backgroundImage = [ 
+    //     'https://i.giphy.com/media/paMdZlRXlc96o/giphy.webp',
+    //     'https://78.media.tumblr.com/98ee23de0b77f7a91d0802dc2ffd3ff6/tumblr_n62bti3Cg81swm1iso1_1280.gif',
         
-        ]
+    //     ]
         
-        function display (F,C){
+        function displayTemp (F,C){
             if (C) return Math.round((F-32)*(5/9)) * '&deg; C';
             return Math.round(F) + '&deg; F';
         }
         
+        function render (data, C){
+            var currentWeather = data.weather[0].description;
+            var currentTemp = displayTemp(data.main.temp,C);
+            var icon = data.weather[0].icon;
+            
+            
+            $('#currentTemp').html(currentTemp);
+            $('#currentWeather').html(currentWeather);
+            
+            var apiIcon ='https://openweathermap.org/img/w/' + icon + '.png';
+            $('#currentTemp').prepend ('<img src=' + apiIcon + '>');
+        }
+        
+        console.log(apiIcon)
+        
         $.getJSON('https://freegeoip.net/json/').done(function(location) {
-            console.log(location);
+            // console.log(location);
             
             
         $('#country').html(location.country_name);
@@ -26,17 +41,25 @@ $(function(){
         $('#longitude').html(location.longitude);
 
 
-        var lat= location.latitude
-        var lon= location.longitude
+        // var lat= location.latitude
+        // var lon= location.longitude
         
-        $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=imperial'+APIKEY,
+        $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+location.latitude + '&lon='+ location.longitude +'&units=imperial'+APIKEY,
         function(data){
             apiData=data;
-            console.log(apiData);
+            // console.log(apiData);
             // console.log(APIKEY);
-        })
+            
+            // render(apiData,C);
+            
+            
+            $('#toggle').click(function(){
+                C= !C;
+                render(data,C);
+            });
+        });
         
-        })
+        });
         
         
-})
+});
