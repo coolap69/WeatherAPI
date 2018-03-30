1c3acd79293600f5ed1e0e4eae3e00d4fa2fba5f
Louis Pazos <louispazos@gmail.com>
2018-03-30 01:23:17 +0000

    temperature works

--- index.js
+++ index.js
@@ -4,31 +4,34 @@ $(function(){
     var C = false;
     var apiData;
     
-    // backgroundImage = [ 
+    // backgroundImg = [ 
     //     'https://i.giphy.com/media/paMdZlRXlc96o/giphy.webp',
     //     'https://78.media.tumblr.com/98ee23de0b77f7a91d0802dc2ffd3ff6/tumblr_n62bti3Cg81swm1iso1_1280.gif',
-        
+    //     'http://farmersalmanac.com/wp-content/uploads/2015/06/Thunderstorm-5best.jpg',
+    //     'http://c1.staticflickr.com/8/7393/11375147803_0f453d8676_b.jpg'
     //     ]
         
-        function displayTemp (F,C){
+        function display (F,C){
             if (C) return Math.round((F-32)*(5/9)) * '&deg; C';
             return Math.round(F) + '&deg; F';
         }
         
         function render (data, C){
             var currentWeather = data.weather[0].description;
-            var currentTemp = displayTemp(data.main.temp,C);
+            var currentTemp = display(data.main.temp,C);
             var icon = data.weather[0].icon;
             
             
             $('#currentTemp').html(currentTemp);
             $('#currentWeather').html(currentWeather);
+            // $('#icon').html(currentWeather);
+
             
-            var apiIcon ='https://openweathermap.org/img/w/' + icon + '.png';
-            $('#currentTemp').prepend ('<img src=' + apiIcon + '>');
+            var apiIcon ='https://openweathermap.org/img/w/' + icon + '.png ';
+            $('#icon').html ('<img src=' + apiIcon + '>');
+               // console.log(apiIcon)
         }
         
-        console.log(apiIcon)
         
         $.getJSON('https://freegeoip.net/json/').done(function(location) {
             // console.log(location);
@@ -50,7 +53,7 @@ $(function(){
             // console.log(apiData);
             // console.log(APIKEY);
             
-            // render(apiData,C);
+            render(apiData,C);
             
             
             $('#toggle').click(function(){
